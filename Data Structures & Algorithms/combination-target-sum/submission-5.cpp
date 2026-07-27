bff781b52ee5b55747e923aa8df094dd183e7c4b
yoimdepressed <somisetty.yogansh@students.iiit.ac.in>
2026-07-27 15:29:38 +0530

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    void solve(vector<int>& nums,int idx, int sum) {
+        if(sum == 0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else if(sum < 0 || idx == nums.size()) return;
+        temp.push_back(nums[idx]);
+        solve(nums,idx,sum-nums[idx]);
+        temp.pop_back();
+        solve(nums,idx+1,sum);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        solve(nums,0,target);
+        return ans;
+    }
+};
