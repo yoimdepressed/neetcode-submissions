0318242bb9bdb02f3625ca3f49002ed5431a0698
yoimdepressed <somisetty.yogansh@students.iiit.ac.in>
2026-08-01 22:18:52 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+        for(int i = nums.size()-2;i >= 0;i--)
+        {
+            if(nums[i] - goal+i >= 0) goal = i;
+        }
+        if(goal == 0) return true;
+        return false;
+    }
+};
