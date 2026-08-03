57ce04dfbfcb0982fddc6eb6fa6c2a76ff9ebe01
yoimdepressed <somisetty.yogansh@students.iiit.ac.in>
2026-08-03 12:35:16 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sumsquares(int n) {
+        int sum = 0;
+        while(n != 0)
+        {
+            sum += (n%10)*(n%10);
+            n = n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        vector<int> visited(244,0);
+        while(n != 1)
+        {
+            n = sumsquares(n);
+            if(!visited[n])
+                visited[n] = 1;
+            else if(visited[n] == 1) return false;
+            if(n == 1) return true;
+            if(n == 2) return false;
+
+        }
+        return true;
+    }
+};
